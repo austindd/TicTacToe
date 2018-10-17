16dc700a85314cf71d4db3d6a3a758c6cb4037cf
Austin Davis <austindanieldavis@gmail.com>
2018-10-16 22:19:35 -0500

    Finished JS instructional videos. About to start lab.

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
-console.log('Start If/Then & Boolean Ops');
+console.log('Start Program')
+
+// GLOBAL VARIABLE DECLARATIONS
 var friends = ['Matt', 'Will', 'Jackson', 'David', 'Paul', 'Kimberly', 'Jio'];
 var val = 55;
+
+/*
+
+console.log('Start If/Then & Boolean Ops');
 
 if(val == 55) {
     console.log('True');
@@ -21,7 +27,7 @@ for(var i = 0; i < friends.length; i++) {
         break;
     }
 
-};
+}
 
 console.log('');
 console.log('Start While Loop');
@@ -30,7 +36,20 @@ var count = 0;
 while(count < friends.length) {
     console.log(friends[count]);
     count++;
-};
+}
 
-console.log('')
+*/
+
+function add(num1, num2) {
+    console.log('Addition Function Used');
+    return num1 + num2;
+}
+
+var sum = add(221, 34);
+console.log(sum);
+
+console.log(add(10, 15));
+
+
+console.log('');
 console.log('END PROGRAM');
