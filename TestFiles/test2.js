1d2bc99484be186b2ca1f2cf6399bddbaf185263
Austin Davis <austindanieldavis@gmail.com>
2018-10-17 15:36:52 -0500

    learned about event handling in JS. Organized folder a bit.

---
+++ TestFiles/test2.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector('button');
+
+btn.addEventListener("click", function() {
+    alert('Hello World!');
+});
+
+let cells = document.querySelectorAll('.cell')
+
+cells.forEach(function(cell) {
+    cell.addEventListener("click", cellClicked);
+});
+
+function cellClicked(e) {
+    e.target.innerHTML = 'Hello';
+}
