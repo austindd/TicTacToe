88623205c4bc1f80bb87e2159c65e459ef9cf941
Austin Davis <austindanieldavis@gmail.com>
2018-10-17 23:58:30 -0500

    game board now displays 'X' and 'O' in the cell for the appropriate turn.

--- TicTacToeFiles/app.js
+++ TicTacToeFiles/app.js
@@ -1,4 +1,6 @@
 
+let userTurn = 0;
+
 // Adding event listener to hear clicks on TicTacToe board
 let cells = document.querySelectorAll('.cell');
 
@@ -6,24 +8,36 @@ cells.forEach(function(cell) {
     cell.addEventListener("click", cellClicked);
 });
 
+// MAIN GAME STRUCTURE
+
 // When a cell is clicked, cellClicked() returns list of classes for that cell
 function cellClicked(e) {
-    let cellData = e.target.classList;
-    console.log(cellData);
-    console.log(cellData[0]);
-    console.log(cellData[1]);
-    console.log(cellData[2]);
-    e.target.innerHTML = cellOutput();
+    if (userTurn < 8){
+
+        let cellData = e.target.classList;
+        console.log(cellData);
+        console.log(cellData[0]);
+        console.log(cellData[1]);
+        console.log(cellData[2]);
+        e.target.innerHTML = cellOutput(userTurn);
+        ++userTurn;
+
+    } else if (userTurn <= 8){
+        winLossTie()
+    }
 };
 
-// Changes the cell to read 'X' or 'O' based on which turn. Stores value in array for win/loss testing.
-let userTurn = 0;
-
-function cellOutput(cellData) {
+// cellOutput(userTurn) changes the cell to read 'X' or 'O' based on which turn.
+// Stores value in array for win/loss testing.
+function cellOutput(userTurn) {
 
     if (userTurn % 2 == 0) {
+        console.log('userTurn =', userTurn);
+        ++userTurn;
         return 'X';
     } else if (userTurn % 2 == 1) {
+        console.log('userTurn =', userTurn);
+        ++userTurn;
         return 'O';
     } else {
         console.log('Error: userTurn undefined');
@@ -31,11 +45,7 @@ function cellOutput(cellData) {
 };
 
 
-// MAIN GAME STRUCTURE
-
-console.log('Game Start');
-
-
+// winLossTie() analyzes the game board to determine if a win, loss, or tie has occurred.
 
 
 console.log('Game Over')
