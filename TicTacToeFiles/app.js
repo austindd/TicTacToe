8d35e28a23582ba8e5e3c01051234fb59e6d0377
Austin Davis <austindanieldavis@gmail.com>
2018-10-18 00:41:32 -0500

    Playing around with pushing cell class info into an array for win/loss/tie analysis. Also minor corrections.

--- TicTacToeFiles/app.js
+++ TicTacToeFiles/app.js
@@ -1,5 +1,10 @@
-
+// userTurn will increment up to 8 (end of game) within the cellClicked() function.
 let userTurn = 0;
+
+// cellClassArray is an object that contains cell objects and their properties.
+// Used for win/loss/tie analysis.
+let cellClassArray = [];
+
 
 // Adding event listener to hear clicks on TicTacToe board
 let cells = document.querySelectorAll('.cell');
@@ -12,20 +17,27 @@ cells.forEach(function(cell) {
 
 // When a cell is clicked, cellClicked() returns list of classes for that cell
 function cellClicked(e) {
-    if (userTurn < 8){
+    if (userTurn <= 8){
 
         let cellData = e.target.classList;
+        cellClassArray.push(cellData[0], cellData[1], cellData[2]);
         console.log(cellData);
         console.log(cellData[0]);
         console.log(cellData[1]);
         console.log(cellData[2]);
+        console.log('cellClassArray contains', cellClassArray);
         e.target.innerHTML = cellOutput(userTurn);
         ++userTurn;
 
-    } else if (userTurn <= 8){
-        winLossTie()
+    } else if (userTurn > 8){
+        //winLossTie();
+        alert('Game Over');
     }
 };
+
+if (userTurn == 8) {
+    alert('Game Over (outside of function)')
+}
 
 // cellOutput(userTurn) changes the cell to read 'X' or 'O' based on which turn.
 // Stores value in array for win/loss testing.
@@ -46,6 +58,6 @@ function cellOutput(userTurn) {
 
 
 // winLossTie() analyzes the game board to determine if a win, loss, or tie has occurred.
+function winLossTie(cellClassArray) {
 
-
-console.log('Game Over')
+}
